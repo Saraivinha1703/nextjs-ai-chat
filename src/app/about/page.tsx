0be22b36ad10f81c676236e8dc17c2c815af2d4d
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-04-10 09:12:36 +0100

    📦 NEW: About page content (make mdx in the future)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,19 @@
 export default function AboutPage() {
     return (
-        <div>
-            <h1>About the Chat</h1>
-        </div>
-    )
+      <div>
+        <h1>About the Chat</h1>
+        <p>
+          The main chat is an open-source chat using Hugging Face Inference API
+          to get the Open Assistant model. It is very simple to implement a chat
+          with Vercel&apos;s AI SDK, with any provider, TogetherAI, Anthropic,
+          OpenAI, Hugging Face, LangChain and many others.
+        </p>
+        <p>
+          However, it&apos;s not that simple to implement the function calling
+          feature, it&apos;s available in a few LLMs and AI SDK only supports
+          some of them. You have other ways to stream UI and text information
+          using their other APIs.
+        </p>
+      </div>
+    );
 }
