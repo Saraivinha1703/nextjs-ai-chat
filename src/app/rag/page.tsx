d6aab6075c5114ab51b6444fa9f4fd9eca78cb04
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-04-15 22:08:50 +0100

    📦 NEW: Working on RAG with Supabase, OpenAI and LangChain

---
+++ src/app/rag/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+"use strict"; //https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
+
+export default function RAGPage() {
+    return (
+        <div>
+        </div>
+    );
+}
