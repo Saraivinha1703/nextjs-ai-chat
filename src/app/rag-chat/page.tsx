e2d6cc9d519ca27fe21bf63beb213eaa1264cfc4
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-04-17 15:36:41 +0100

    📦 NEW: Adding documents

--- src/app/rag-chat/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-"use strict"; //https://stackoverflow.com/questions/1335851/what-does-use-strict-do-in-javascript-and-what-is-the-reasoning-behind-it
-
-export default function RAGPage() {
-    return (
-        <div>
-        </div>
-    );
-}
